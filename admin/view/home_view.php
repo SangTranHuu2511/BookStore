c76519d95efe90a03106c02f08a80a66dd0df880
Tran Huu Sang <Tran Huu Sang>
2017-01-14 14:21:46 +0700

    sang update

--- admin/view/home_view.php
+++ admin/view/home_view.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <div class="main-content">
-              <h1 class="text-center">Chào mừng bạn đến với trang quản trị</h1>
+              <h1 class="text-center">Chào mừng Admin</h1>
               <img src="../../public/images/homeadmin.jpg" alt="" width="100%" height="100%">
             </div>
         </div>
